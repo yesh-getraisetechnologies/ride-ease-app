6384d15c9ee22b8984a73c21e156827ab0ed66c4
Rohan Vijay <rohanvijay@Rohans-MacBook-Air.local>
2024-06-24 17:09:22 +0530

    fix: Home UI

--- app/screen/routeForRide.jsx
+++ app/screen/routeForRide.jsx
@@ -1,24 +1,72 @@
-import React from "react";
-import {
-    StyleSheet,
-    Text,
-    View,
-    Pressable,
-  } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function RouteForRide() {
+const RouteForRide = () => {
   return (
-    <View>
-      <Text>RouteForRide</Text>
-      <Text>A S Rao Nagar </Text>
-      <Text>|</Text>
-      <Text>Sainik Puri </Text>
-      <Text>|</Text>
-      <Text>A S Rao Nagar </Text>
-      <Text>|</Text>
-      <Text>Vayupuri </Text>
-      <Text>|</Text>
-      <Text>Neredmet </Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Icon name="location-on" size={20} color="#65696D" />
+        <Text style={styles.text}>A S Rao Nagar</Text>
+        <Icon name="navigation" size={20} color="black" />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>|</Text>
+      </View>
+      <View style={styles.row}>
+        <Icon name="location-on" size={20} color="#65696D" />
+        <Text style={styles.text}>Sainik Puri</Text>
+        <Icon name="navigation" size={20} color="blue" />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>|</Text>
+      </View>
+      <View style={styles.row}>
+        <Icon name="location-on" size={20} color="#65696D" />
+        <Text style={styles.text}>A S Rao Nagar</Text>
+        <Icon name="navigation" size={20} color="blue" />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>|</Text>
+      </View>
+      <View style={styles.row}>
+        <Icon name="location-on" size={20} color="#65696D" />
+        <Text style={styles.text}>Vayupuri</Text>
+        <Icon name="navigation" size={20} color="blue" />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>|</Text>
+      </View>
+      <View style={styles.row}>
+        <Icon name="location-on" size={20} color="#65696D" />
+        <Text style={styles.text}>Neredmet</Text>
+        <Icon name="navigation" size={20} color="blue" />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>|</Text>
+      </View>
+      <View style={styles.row}>
+      <Icon name="navigation" size={20} color="white"  border="3px" borderColor="blue"/>
+        <Text style={styles.text}>Hyd_GoogleGDO_Sar2</Text>
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  text: {
+    marginLeft: 5,
+    fontSize: 16,
+    color: 'black',
+  },
+});
+
+export default RouteForRide;
